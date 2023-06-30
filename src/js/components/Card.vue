aa3f33f39ae7a52c30d2c74d3c232a462c987451
Andres Cayambe <andrescayambe99@hotmail.com>
2023-06-30 07:04:03 +0200

    [add]buttons-categories-tag

--- src/js/components/Card.vue
+++ src/js/components/Card.vue
@@ -1,17 +1,16 @@
 <script>
-
 import { store } from "../../store/store.js";
-import  axios  from 'axios';
+import axios from "axios";
 
 export default {
   name: "Card",
-  
-  props:{
-    project: Object
+
+  props: {
+    project: Object,
   },
 
-  computed:{
-    formattedData(){
+  computed: {
+    formattedData() {
       const day = new Date(this.project.creation_date);
       const options = {
         weekday: "long",
@@ -19,13 +18,13 @@ export default {
         month: "long",
         day: "numeric",
       };
-      return day.toLocaleString('it-IT', options)
+      return day.toLocaleString("it-IT", options);
     },
-    category(){
-      if(!this.project.category_id) return '-no category-';
-      return this.project.category_id.name
-    }
-  }
+    category() {
+      if (!this.project.category) return "-no category-";
+      return this.project.category.name;
+    },
+  },
 };
 </script>
 
@@ -36,20 +35,44 @@ export default {
         <h1>{{ project.name }}</h1>
         <p>{{ project.description }}</p>
         <span>{{ formattedData }}</span>
-        <span>{{ category }}</span>
-
-
+        <div class="container-small">
+          <div class="btn btn-category">{{ category }}</div>
+          <ul>
+            <li v-for="tag in project.tags" :key="tag.id" class="btn btn-tags">
+              {{ tag.name }}
+            </li>
+          </ul>
+        </div>
       </div>
     </li>
   </ul>
 </template>
 
 <style lang="scss" scoped>
-ul{
+ul {
   list-style: none;
-  li{
+  li {
+    display: flex;
     padding: 20px;
     border-bottom: 1px solid black;
+  }
+}
+.container-small {
+ min-width: 60px ;
+  .btn {
+    display: flex;
+    padding: 20px 20px;
+    font-weight: 800;
+    border-radius: 20px;
+    margin: 10px;
+  }
+
+  .btn-category {
+    background-color: white;
+  }
+  .btn-tags {
+    background-color: black;
+    color: white;
   }
 }
 </style>
